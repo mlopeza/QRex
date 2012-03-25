1d88f1b632fa85801ff3b142c4b50b49d7b88450
Luis Aviles <lenwe.db@gmail.com>
2012-03-24 18:12:19 -0600

    Added Cuadruplo class

--- TDV.h
+++ TDV.h
@@ -9,179 +9,196 @@
 #define PROCSTRING 8000
 namespace std
 {
-    typedef basic_string<char> string;
-    typedef basic_string<wchar_t> wstring;
+	typedef basic_string<char> string;
+	typedef basic_string<wchar_t> wstring;
 };
 
 namespace QRex{
-/*Variables*/
-class Variable{
-	public:
-		std::wstring nombre;
-		int tipo;
-		int direccion;
+	/*Variables*/
+	class Variable{
+		public:
+			std::wstring nombre;
+			int tipo;
+			int direccion;
 
-	Variable(){
-		tipo = -1;
-		direccion = -1;
-	}
+			Variable(){
+				tipo = -1;
+				direccion = -1;
+			}
 
-	~Variable(){
-	}
-		
-};
+			~Variable(){
+			}
 
-/*Linked List de Parametros*/
-class Node {
-	int data;
-	Node* next;
+	};
 
-  public:
-	Node() {data = -1; next = NULL;};
-	~Node(){}
-	void SetData(int aData) { data = aData; };
-	void SetNext(Node* aNext) { next = aNext; };
-	int Data() { return data; };
-	Node* Next() { return next; };
+	/*Linked List de Parametros*/
+	class Node {
+		int data;
+		Node* next;
 
-};
+		public:
+		Node() {data = -1; next = NULL;};
+		~Node(){}
+		void SetData(int aData) { data = aData; };
+		void SetNext(Node* aNext) { next = aNext; };
+		int Data() { return data; };
+		Node* Next() { return next; };
 
-class List {
-	public:
-		Node *head;
-		int size;
+	};
 
-		List() { head = NULL; size = 0;};
-		void Append(int data){
-			// Crea nuevo nodo
-			Node* newNode = new Node();
-			newNode->SetData(data);
-			newNode->SetNext(NULL);
-		
-			// Crea pointer temporal
-			Node *tmp = head;
-		
-			if ( tmp != NULL ) {
-				// Hay nodos en la Lista
-				// Busca hasta llegar al final de la lista
-				while ( tmp->Next() != NULL ) {
+	class List {
+		public:
+			Node *head;
+			int size;
+
+			List() { head = NULL; size = 0;};
+			void Append(int data){
+				// Crea nuevo nodo
+				Node* newNode = new Node();
+				newNode->SetData(data);
+				newNode->SetNext(NULL);
+
+				// Crea pointer temporal
+				Node *tmp = head;
+
+				if ( tmp != NULL ) {
+					// Hay nodos en la Lista
+					// Busca hasta llegar al final de la lista
+					while ( tmp->Next() != NULL ) {
+						tmp = tmp->Next();
+					}
+
+					// Asigna al ultimo nodo el nuevo nodo
+					tmp->SetNext(newNode);
+				}else {
+					// Asigna el primer Nodo
+					head = newNode;
+				}
+				size++;
+			};
+
+			void Print(){
+				Node *tmp = head;
+				std::wcout<< L"Parametros:";
+				while(tmp != NULL){
+					std::wcout<< L"("<< tmp->Data() << L")" <<L"   ";
+					tmp=tmp->Next();
+				}
+
+				std::wcout<< L"\n";
+			};
+
+			void Delete(){
+				Node *tmp = head;
+				Node *tmp2;
+				while(tmp != NULL){
+					tmp2=tmp->Next();
+					delete(tmp);
+					tmp=tmp2;
+				}
+			};
+
+			bool Equals(List *lista){
+				Node *tmp = head;
+				Node *x = lista->head;
+
+				//Tienen la misma cantidad de parametros?
+				if(lista->size != this->size)
+					return false;
+
+				//Checa si son del mismo tipo
+				while(tmp != NULL){
+
+					if(x->Data() != tmp->Data())
+						return false;
+					x = x->Next();
 					tmp = tmp->Next();
 				}
-		
-			// Asigna al ultimo nodo el nuevo nodo
-			tmp->SetNext(newNode);
-			}else {
-				// Asigna el primer Nodo
-				head = newNode;
-			}
-			size++;
-		};
 
-		void Print(){
-			Node *tmp = head;
-			std::wcout<< L"Parametros:";
-			while(tmp != NULL){
-				std::wcout<< L"("<< tmp->Data() << L")" <<L"   ";
-				tmp=tmp->Next();
-			}
+				//Los parametros son iguales
+				return true;
+			};
+	};
 
-			std::wcout<< L"\n";
-		};
+	/*Funciones*/
+	class FuncionX{
+		public:
+			std::wstring nombre;
+			int tipo;
+			int definida;
+			List *parametros;
+			std::map<std::wstring, Variable *> vhash;
+			int intNum;
+			int floatNum;
+			int stringNum;
 
-		void Delete(){
-			Node *tmp = head;
-			Node *tmp2;
-			while(tmp != NULL){
-				tmp2=tmp->Next();
-				delete(tmp);
-				tmp=tmp2;
-			}
-		};
-
-		bool Equals(List *lista){
-			Node *tmp = head;
-			Node *x = lista->head;
-
-			//Tienen la misma cantidad de parametros?
-			if(lista->size != this->size)
-				return false;
-
-			//Checa si son del mismo tipo
-			while(tmp != NULL){
-
-				if(x->Data() != tmp->Data())
-					return false;
-				x = x->Next();
-				tmp = tmp->Next();
+			FuncionX(){
+				tipo = 0;
+				definida = 0;
+				parametros = new List();
+				intNum = PROCINT-1;
+				floatNum = PROCFLOAT-1;
+				stringNum = PROCSTRING-1;
 			}
 
-			//Los parametros son iguales
-			return true;
-		};
-};
+			void Insert(int n){
+				parametros->Append(n);
+			}
 
-/*Funciones*/
-class FuncionX{
-	public:
-		std::wstring nombre;
-		int tipo;
-		int definida;
-		List *parametros;
-		std::map<std::wstring, Variable *> vhash;
-		int intNum;
-		int floatNum;
-		int stringNum;
+			~FuncionX(){
+				parametros->Delete();
+				delete(parametros);
+				vhash.clear();
+			}
 
-	FuncionX(){
-		tipo = 0;
-		definida = 0;
-		parametros = new List();
-		intNum = PROCINT-1;
-		floatNum = PROCFLOAT-1;
-		stringNum = PROCSTRING-1;
-	}
+	};
 
-	void Insert(int n){
-		parametros->Append(n);
-	}
+	/*Contexto, se utiliza para variables globales
+	  y constantes*/
+	class Contexto{
+		public:
+			std::map<std::wstring, Variable *> vhash;
+			int intNum;
+			int floatNum;
+			int stringNum;
 
-	~FuncionX(){
-		parametros->Delete();
-		delete(parametros);
-		vhash.clear();
-	}
+			Contexto(int enteros,int flotantes,int cadenas){
+				intNum = enteros;
+				floatNum = flotantes;
+				stringNum = cadenas;
+			}
 
-};
+			~Contexto(){
+				vhash.clear();
+			}
 
-/*Contexto, se utiliza para variables globales
-	y constantes*/
-class Contexto{
-	public:
-		std::map<std::wstring, Variable *> vhash;
-		int intNum;
-		int floatNum;
-		int stringNum;
+	};
 
-	Contexto(int enteros,int flotantes,int cadenas){
-		intNum = enteros;
-		floatNum = flotantes;
-		stringNum = cadenas;
-	}
+	/*Definicion de la Tabla de Variables*/
+	class TablaDeVariables{
+		public:
+			std::map<std::wstring, FuncionX *> fhash;
 
-	~Contexto(){
-		vhash.clear();
-	}
+			TablaDeVariables(){}
+			~TablaDeVariables(){fhash.clear();}
+	};
 
-};
+	class Cuadruplo{
+		public:
+			int op;
+			int operador1;
+			int operador2;
+			int resultado;
 
-/*Definicion de la Tabla de Variables*/
-class TablaDeVariables{
-	public:
-		std::map<std::wstring, FuncionX *> fhash;
-		
-		TablaDeVariables(){}
-		~TablaDeVariables(){fhash.clear();}
-};
+			Cuadruplo(){
+				op = -1;
+				operador1 = -1;
+				operador2 = -1;
+				resultado = -1;
+			}
+
+			~Cuadruplo(){}
+	};
 
 };
 #endif
